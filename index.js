abdb482d77ec88c7828756de7ca904c21b3c8ebf
Michael <palumbo9673@gmail.com>
2025-04-14 15:11:06 +1000

    added routes

--- index.js
+++ index.js
@@ -2,6 +2,37 @@ const express = require('express') // CommonJS modules
 const app = express()
 const port = 3000
 
+const person = {
+    name: "Mike",
+    age: 28
+}
+
+const posts = [
+    {
+        title: 'post 1',
+        body: 'lorem ipsum',
+        isPublished: false
+    },
+    {
+        title: 'post 2',
+        body: 'this is a second post',
+        isPublished: true
+    }
+]
+
+// Posts routes
+
+// Get all posts
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+// get one post
+app.get('/posts/0', (req, res) => {
+    res.send(posts(0))
+})
+// create a new post
+// update post
+// delete post
 
 // Python/Flask
 // @app.route('/') // default get
@@ -11,7 +42,8 @@ const port = 3000
 // app.[http-verb]([path], [callback])
 // Callback accepts a request and response object
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    // Serialize person and send to the client
+    res.send(person)
 })
 
 // Start the dev serer on the given port
